28c78c3b12003c972ca0d51fdbf8185b889863b8
Cecilia <ceci_2504@hotmail.com>
2025-04-20 18:37:36 +0200

    setup MySQL connection using Sequelize and dotenv. Define user model with sequelize

---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import connection from "../config/sequelize.js";
+
+const User = connection.define("user", {
+    user_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_email :{
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true,
+    },
+    user_pwd: {
+        type: DataTypes.STRING(80),
+        allowNull: false,
+    },
+    user_name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        unique: true,
+    }
+
+})

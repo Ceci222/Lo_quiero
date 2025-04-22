1d4f814e2e699e592c435767efd41b3c75c1896a
Cecilia <ceci_2504@hotmail.com>
2025-04-22 20:38:06 +0200

    rename pickupRouter, fix and test app.js, add apiRouter.js with routes for users, objects, and pickups

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "../routes/api/apiRouter.js";
 
 dotenv.config();
 
 const APP_PORT = process.env.APP_PORT;
 const app = express();
 
-app.listen(APP_PORT, () => {
+app.use("/",router);
+
+app.get('/test', (req, res) => {  
+    res.send('¡Funciona!');
+});
+
+app.listen(3000, '0.0.0.0', () => { //TODO: verificar sin los ceros
     console.log(`Backend conectado al puerto ${APP_PORT}`);
 });
 
---
+++ src/routes/api/apiRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import userRouter from "./userRouter.js";
+import objectRouter from "./objectRouter.js";
+import pickupRouter from "./pickupRouter.js";
+/*import authRouter from "./authRouter.js";*/
+ 
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.send("Testing app");
+})
+
+router.use("/user", userRouter);
+router.use("/object",objectRouter);
+router.use("/pickup", pickupRouter);
+/*router.use("/", authRouter);*/
+
+export default router;
